121d3f9d65c764b2036c89098a066ac63d00e358
Sergey M (Ageers) <ageers@mail.ru>
2025-07-06 21:09:18 +0300

    solution ex2.6 don't finish

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int days = 234;
-        int period = 7;
-        int fullWeek = days / period;
-        System.out.println(fullWeek);
+        int secFromStart = 21002;
+        int hoursFromStart = secFromStart / 3600;
+        int minFromStartLastHour = (secFromStart % 3600) / 60;
+        int secFromStartLastMinutes = minFromStartLastHour / 60;
+
+
+        System.out.println("S nachala sutok proshlo: " + hoursFromStart + " chasov");
+        System.out.println("S nachala chasa proshlo: " + minFromStartLastHour + " minutes");
+        System.out.println("S nachala posledney minuy prosholo: " + secFromStartLastMinutes + " secund");
+
 
     }
 }
